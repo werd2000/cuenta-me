90ef8c6bce8fc55f54d6677cd0c416ac12f947d3
werd2000 <walter.enrique.ruiz.diaz@gmail.com>
2020-10-26 18:30:51 -0300

    Esquema básico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { GraficaComponent } from './pages/grafica/grafica.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    GraficaComponent,
+    NopagefoundComponent,
+    ProgressComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
